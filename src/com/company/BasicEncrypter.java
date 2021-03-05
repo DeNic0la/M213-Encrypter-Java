dc1493fd370b210863b693c6c5b81d9a03db3294
DeNic0la <57986114+DeNic0la@users.noreply.github.com>
2021-03-05 14:00:52 +0000

    Made the Possibly worst encryption elgorythm

--- src/com/company/BasicEncrypter.java
+++ src/com/company/BasicEncrypter.java
@@ -3,13 +3,19 @@ package com.company;
 public class BasicEncrypter implements Encrypter {
     @Override
     public byte[] encrypt(byte[] data) {
-        // TODO: Edit this Function
-        return data;
+        byte[] enc = new byte[data.length];
+        for (int i = 0; i < data.length; i++) {
+            enc[i] = (byte) (i % 2 == 0 ? data[i] - 1 : data[i] + 1);
+        }
+        return enc;
     }
 
     @Override
     public byte[] decrypt(byte[] data) {
-        // TODO: Edit this Function
-        return data;
+        byte[] dec = new byte[data.length];
+        for (int i = 0; i < data.length; i++) {
+            dec[i] = (byte) (i % 2 == 0 ? data[i] - 1 : data[i] + 1);
+        }
+        return dec;
     }
 }
