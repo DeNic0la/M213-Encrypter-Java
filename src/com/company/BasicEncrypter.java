ed6f9633b5904c22c9f2fa1d6d43c8ea72f5971f
DeNic0la <57986114+DeNic0la@users.noreply.github.com>
2021-03-05 13:38:07 +0000

    Added Helpfull Comments

--- src/com/company/BasicEncrypter.java
+++ src/com/company/BasicEncrypter.java
@@ -3,11 +3,13 @@ package com.company;
 public class BasicEncrypter implements Encrypter {
     @Override
     public byte[] encrypt(byte[] data) {
+        // TODO: Edit this Function
         return data;
     }
 
     @Override
     public byte[] decrypt(byte[] data) {
+        // TODO: Edit this Function
         return data;
     }
 }
