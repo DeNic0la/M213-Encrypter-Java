d979be1dbf289fb8a5d9cc10f0a84338667663f1
DeNic0la <57986114+DeNic0la@users.noreply.github.com>
2021-03-05 13:37:24 +0000

    Finished Preset

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    String myData = new String("Guten Tag, das ist ein Toller Text");
+	    Encrypter myEncrypter = new BasicEncrypter();
+	    String DecryptedData = new String(myEncrypter.encrypt(myData.getBytes()));
+        String EncryptedData = new String(myEncrypter.decrypt(DecryptedData.getBytes()));
+
+        System.out.println(myData);
+        System.out.println(DecryptedData);
+        System.out.println(EncryptedData);
     }
 }
