eb4b9321ea1a2516a33aab79cfeddbe2563a283d
DeNic0la <57986114+DeNic0la@users.noreply.github.com>
2021-03-17 16:56:54 +0000

    Upgraded Encryption Algorythm

--- src/com/company/BasicEncrypter.java
+++ src/com/company/BasicEncrypter.java
@@ -3,18 +3,25 @@ package com.company;
 public class BasicEncrypter implements Encrypter {
     @Override
     public byte[] encrypt(byte[] data) {
+        int toAdd = data.length % 5;
+        int toSubstract = data.length % 3;
+
         byte[] enc = new byte[data.length];
         for (int i = 0; i < data.length; i++) {
-            enc[i] = (byte) (i % 2 == 0 ? data[i] - 1 : data[i] + 1);
+            enc[i] = (byte) (i % 2 == 0 ? data[i] - toSubstract : data[i] + toAdd);
+            enc[i] = (byte) (i%3==0?enc[i]+ toAdd : enc[i] - toSubstract);
         }
         return enc;
     }
 
     @Override
     public byte[] decrypt(byte[] data) {
+        int toAdd = data.length % 3;
+        int toSubstract = data.length % 5;
         byte[] dec = new byte[data.length];
         for (int i = 0; i < data.length; i++) {
-            dec[i] = (byte) (i % 2 == 0 ? data[i] + 1 : data[i] - 1);
+            dec[i] = (byte) (i % 2 == 0 ? data[i] + toAdd : data[i] - toSubstract);
+            dec[i] = (byte) (i%3==0?dec[i]- toSubstract : dec[i] + toAdd);
         }
         return dec;
     }
